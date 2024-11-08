b6556577158ba1a0748f4c26e802c457d80052ee
Dhananjaya Dabeer <dabeerdhananja123@gmail.com>
2024-11-08 08:20:01 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ mongoose
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: `${process.env.ORIGIN}`,
     credentials: true,
 }))
 app.use(cookieParser())
